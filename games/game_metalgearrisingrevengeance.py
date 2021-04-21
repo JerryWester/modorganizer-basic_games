380b9749efe248781b375b75162ecd024916fb2e
Jerry Wester <JerryWester@users.noreply.github.com>
2021-04-21 04:00:51 -0600

    Metal Gear Rising: Revengeance Plugin

---
+++ games/game_metalgearrisingrevengeance.py
@@ -0,0 +1,18 @@
+from ..basic_game import BasicGame
+
+
+class MetalGearRisingRevengeanceGame(BasicGame):
+
+    Name = "Metal Gear Rising: Revengeance Support Plugin"
+    Author = "JerryWester"
+    Version = "1.0.0"
+
+    GameName = "Metal Gear Rising: Revengeance"
+    GameShortName = "metalgearrisingrevengeance"
+    GameNexusName = "metalgearrisingrevengeance"
+    GameSteamId = 235460
+    GameBinary = "METAL GEAR RISING REVENGEANCE.exe"
+    GameDataPath = "GameData"
+    GameDocumentsDirectory = "%DOCUMENTS%/MGR"
+    GameSavesDirectory = "%GAME_DOCUMENTS%/SaveData"
+    GameSaveExtension = "sav"
